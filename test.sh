44af5a88ccbc092bc492ce883e2f73e8529d1051
ddomizzisg <ddomizzisag@gmail.com>
2025-04-22 20:24:17 +0200

    test

---
+++ test.sh
@@ -0,0 +1,4 @@
+python3 test.py --host 192.5.86.187 --size 1 --repeat 33
+python3 test.py --host 192.5.86.187 --size 10 --repeat 33
+python3 test.py --host 192.5.86.187 --size 100 --repeat 33
+python3 test.py --host 192.5.86.187 --size 1000 --repeat 10
